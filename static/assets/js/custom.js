d9fa8734d7cf6dc7eb3e7446e1b026cf59c48384
Jorge Oehrens <jorge.oehrens@gmail.com>
2022-08-05 12:05:04 -0400

    'update'

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function($) {
 
     var latitude = 34.038405;
     var longitude = -117.946944;
-    var markerImage = "assets/img/map-marker.png";
+    var markerImage = "../../static/assets/img/map-marker.png";
     var mapElement = "map";
     var mapStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dbdbdb"},{"visibility":"on"}]}];
     if( $("#map").length ){
