ebe3eaab13ed0aaad839441598c91de7282931a5
Jorge Oehrens <jorge.oehrens@gmail.com>
2022-08-05 11:08:47 -0400

    'update'

--- webcertificado/urls.py
+++ webcertificado/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
 
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG: 
+    urlpatterns += patterns('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}), )
